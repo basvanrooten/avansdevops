6695c874c7054da23884d1cb06d87c08ff7cb0c5
Bas van Rooten <basvanrooten@me.com>
2021-02-28 13:31:57 +0100

    Remove code program.cs

--- AvansDevOps/AvansDevOps/Program.cs
+++ AvansDevOps/AvansDevOps/Program.cs
@@ -13,24 +13,6 @@ namespace AvansDevOps
     {
         static void Main(string[] args)
         {
-            Person p1 = new Person("Bas");
-            Person p2 = new Person("Tom");
-            Project project = new Project("Test Project", p1);
-
-            project.AddSprint(new ReviewSprint("Review Sprint 1", DateTime.Now, DateTime.Now.AddDays(1), project, p1, new List<Person>() { p2 }));
-            ISprint sprint = project.GetSprints().First();
-            Console.WriteLine(sprint.GetName());
-            sprint.GetState().SetName("Changed Name");
-            sprint.GetState().startStateAction();
-
-            Console.WriteLine(sprint.GetName());
-            Console.ReadLine();
-
-        }
-
-        public static string HelloWorld()
-        {
-            return "Hello World";
         }
     }
 }
