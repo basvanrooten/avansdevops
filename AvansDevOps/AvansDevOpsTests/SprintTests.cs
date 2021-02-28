1a239c7c28c0b89f9564274fe0b65d8c24acb720
Bas van Rooten <basvanrooten@me.com>
2021-02-28 13:26:37 +0100

    Add tests for start & endDate for InitializedState

--- AvansDevOps/AvansDevOpsTests/SprintTests.cs
+++ AvansDevOps/AvansDevOpsTests/SprintTests.cs
@@ -33,6 +33,52 @@ namespace AvansDevOpsTests
         }
 
         [Fact]
+        public void Changing_StartDate_Should_Not_Throw_Exception_In_InitializedState()
+        {
+            // Arrange
+
+            Project project = new Project("Test Project", new Person("Bas"));
+            SprintFactory factory = new SprintFactory();
+
+            Person p1 = new Person("Tom");
+            Person p2 = new Person("Jan Peter");
+
+            ISprint sprint = factory.MakeReviewSprint("Sprint 1", DateTime.Now, DateTime.Now.AddDays(14), project, p1, new List<Person>() { p2 });
+            project.AddSprint(sprint);
+            // Act
+
+            sprint.GetState().SetStartDate(DateTime.Parse("2010-10-10T00:00:00Z"));
+
+
+            // Assert
+            Assert.Equal(DateTime.Parse("2010-10-10T00:00:00Z"), project.GetSprints().First().GetStartDate());
+
+        }
+
+        [Fact]
+        public void Changing_EndDate_Should_Not_Throw_Exception_In_InitializedState()
+        {
+            // Arrange
+
+            Project project = new Project("Test Project", new Person("Bas"));
+            SprintFactory factory = new SprintFactory();
+
+            Person p1 = new Person("Tom");
+            Person p2 = new Person("Jan Peter");
+
+            ISprint sprint = factory.MakeReviewSprint("Sprint 1", DateTime.Parse("2010-10-10T00:00:00Z"), DateTime.Parse("2010-10-10T00:00:00Z").AddDays(14), project, p1, new List<Person>() { p2 });
+            project.AddSprint(sprint);
+            // Act
+
+            sprint.GetState().SetEndDate(DateTime.Parse("2010-10-10T00:00:00Z").AddDays(7));
+
+
+            // Assert
+            Assert.Equal(DateTime.Parse("2010-10-17T00:00:00Z"), project.GetSprints().First().GetEndDate());
+
+        }
+
+        [Fact]
         public void Set_Review_Should_Throw_NotSupportedException_In_InitializedState()
         {
             // Arrange
