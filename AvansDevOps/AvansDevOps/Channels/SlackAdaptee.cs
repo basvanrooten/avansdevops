d097dc2a86ce7f87dbfd90ae2ee37e811afcad0b
Tom Smits <tmsmits@gmail.com>
2021-02-28 21:34:25 +0100

    Added Channels/Channels adapters

---
+++ AvansDevOps/AvansDevOps/Channels/SlackAdaptee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvansDevOps.Channels
+{
+    public class SlackAdaptee
+    {
+        private readonly string _username;
+
+        public SlackAdaptee(string username)
+        {
+            this._username = username;
+        }
+
+        public void SendMessage(string message)
+        {
+            Console.WriteLine($"Slack message has been sent to: {this._username} Message: {message}");
+        }
+    }
+}
---
+++ AvansDevOps/AvansDevOps/Channels/SlackChannel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvansDevOps.Channels
+{
+    public class SlackChannel : IChannel
+    {
+        private string _username;
+        private readonly SlackAdaptee _adaptee;
+
+        public SlackChannel(string username)
+        {
+            this._username = username;
+            this._adaptee = new SlackAdaptee(this._username);
+        }
+
+        public void SendMessage(string message)
+        {
+            this._adaptee.SendMessage(message);
+        }
+    }
+}
