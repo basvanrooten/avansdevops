e6f57faa5b1dec91acd3666cc55fdab9a122df9d
Bas van Rooten <basvanrooten@me.com>
2021-03-04 22:37:56 +0100

    Add SCM with tests

---
+++ AvansDevOps/AvansDevOps/SCM/GitCommit.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AvansDevOps.Backlogs;
+
+namespace AvansDevOps.SCM
+{
+    public class GitCommit : ICommit
+    {
+        private readonly string _title;
+        private readonly string _description;
+        private readonly BacklogItem _backlogItem;
+
+        public GitCommit(string title, string description, BacklogItem backlogItem)
+        {
+            _title = title;
+            _description = description;
+            _backlogItem = backlogItem;
+        }
+
+
+        public string GetTitle()
+        {
+            return _title;
+        }
+
+        public string GetDescription()
+        {
+            return _description;
+        }
+
+        public BacklogItem GetBacklogItem()
+        {
+            return _backlogItem;
+        }
+    }
+}
---
+++ AvansDevOps/AvansDevOps/SCM/ICommit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AvansDevOps.Backlogs;
+
+namespace AvansDevOps.SCM
+{
+    public interface ICommit
+    {
+        string GetTitle();
+        string GetDescription();
+        BacklogItem GetBacklogItem();
+    }
+}
