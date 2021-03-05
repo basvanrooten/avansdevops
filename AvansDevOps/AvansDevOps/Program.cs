84093c518155b05964de6340a8fff3aa12222fd0
Bas van Rooten <basvanrooten@me.com>
2021-03-05 12:34:51 +0100

    Revert "Remove Program.cs"
    This reverts commit f4315ca222a37d24a30903ff6cadbcc330dca2b0.

---
+++ AvansDevOps/AvansDevOps/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using AvansDevOps.Persons;
+using AvansDevOps.Sprints;
+
+namespace AvansDevOps
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
